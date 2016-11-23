0dab8a57967a6725eacd11f872df50310004dbfe
koudenpa <koudenpa@hotmail.com>
2016-11-23 22:12:40 +0900

    SG-90の仕様に数字を少し合わせてみたがあまり変わらなかった。

--- ServoTestEsp8266/ServoTestEsp8266.ino
+++ ServoTestEsp8266/ServoTestEsp8266.ino
@@ -1,24 +1,19 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com> 
- This example code is in the public domain.
-
- modified 28 May 2015
- by Michael C. Miller
- modified 8 Nov 2013
- by Scott Fitzgerald
-
- http://arduino.cc/en/Tutorial/Sweep
-*/ 
+// Sweep サンプルを元に弄ったものです
+// http://arduino.cc/en/Tutorial/Sweep
 
 #include <Servo.h> 
  
 Servo myservo;  // create servo object to control a servo 
                 // twelve servo objects can be created on most boards
  
-
 void setup() 
 { 
-  myservo.attach(2);  // attaches the servo on GIO2 to the servo object 
+  // attaches the servo on GIO2 to the servo object 
+  // SG-90 を駆動するのでそれ向けに設定する
+  // SG-90 は以下のパルスで動く
+  // -90度（0.5ms） ~ 0度(1.45ms) ~ 90度（2.4ms）、50Hz（20ms単位）
+  // Servo は 0度が0.544ms、180度が2.400ms、がデフォルトなので、デフォルトで良い
+  myservo.attach(2);  
 } 
  
 void loop() 
@@ -28,12 +23,14 @@ void loop()
   for(pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees 
   {                                  // in steps of 1 degree 
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
+    // SG-90 は20ms周期なのでその倍待ってみる
+    // 60度を0.1秒なので、1度動くには1.6ms程度、こちらは問題にならなさそうである
+    delay(40);
   } 
   for(pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
   {                                
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
+    delay(40);
   } 
 } 
 
